b15470703d38e8b7043580a1121943e6a516f073
AbirElM <abirelmokhtari@gmail.com>
2020-10-05 11:51:44 +0100

    http node example

--- NodeJS/node-examples/index.js
+++ NodeJS/node-examples/index.js
@@ -1,17 +1,17 @@
-var rect = require('./rectangle');
+var rect = require('./rectangle.js');
 
-// var rect = {
-// 	perimeter: (x, y) => (2*(x+y)),
-// 	area: (x, y) => (x*y)
-// };
+var rect = {
+	perimeter: (x, y) => (2*(x+y)),
+	area: (x, y) => (x*y)
+};
 
 function solveRect(l,b) {
-    console.log("Solving for rectangle with l = " + l + " and b = " + b);
-
+    console.log("Solving for rectangle with l = "
+                + l + " and b = " + b);
     rect(l,b, (err,rectangle) => {
         if (err) {
-	        console.log("ERROR: ", err.message); 
-	    }
+            console.log("ERROR: ", err.message);
+        }
         else {
             console.log("The area of the rectangle of dimensions l = "
                 + l + " and b = " + b + " is " + rectangle.area());
@@ -20,7 +20,7 @@ function solveRect(l,b) {
         }
     });
     console.log("This statement after the call to rect()");
-}
+};
 
 solveRect(2,4);
 solveRect(3,5);
--- NodeJS/node-examples/rectangle.js
+++ NodeJS/node-examples/rectangle.js
@@ -1,12 +1,10 @@
-// exports.perimeter =  (x, y) => (2*(x+y));
-
-// exports.area = (x, y) => (x*y);
-
 module.exports = (x,y,callback) => {
     if (x <= 0 || y <= 0)
         setTimeout(() => 
             callback(new Error("Rectangle dimensions should be greater than zero: l = "
-                + x + ", and b = " + y), null),2000);
+                + x + ", and b = " + y), 
+            null),
+            2000);
     else
         setTimeout(() => 
             callback(null, {
